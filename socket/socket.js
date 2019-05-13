de49bf0c45293673bb0c0100fb05228a143063f3
张震 <1191@mapgoo.net>
2019-05-13 17:12:18 +0800

    增加聊天室~~~~~~~~~~~~~~~~~

--- socket/socket.js
+++ socket/socket.js
@@ -1,42 +1,84 @@
 var ws = require("nodejs-websocket")
 
 var AllUserData = new Array()
+var userNum = 0
 // Scream server example: "hi" -> "HI!!!"
+
 var server = ws.createServer(function (conn) {
+    userNum++;
+    var myName = '玩家' + userNum;
     AllUserData.push({
-        'user': conn
+        'user': conn,
+        'name': '玩家' + userNum
     })
 
     var sendText = '';
-    console.log(conn)
     for(var i = 0 ; i < AllUserData.length; i++){
-        sendText = JSON.stringify({
-            dataType: 'string',
-            str: "在线用户数量：" + AllUserData.length
-        });
-        console.log(sendText);
-        AllUserData[i].user.sendText(sendText);
+        sendText = {
+            dataType: 'onlineUserNum',
+            onlineUserNum: userNum,
+            cont: ''
+        };
+        if(AllUserData[i].user == conn){
+            sendText.cont = myName + '加入房间，目前房间人数为' + userNum + '人';
+            // console.log(sendText);
+        }else{
+            sendText.cont = myName + '加入房间，目前房间人数为' + userNum + '人';
+        }
+        AllUserData[i].user.sendText(JSON.stringify(sendText));
     }
-    conn.on("text", function (str) {
-        console.log("Received "+str);
+    conn.on("text", function (jsonData) {
+        var obj = {};
         for(var i = 0 ; i < AllUserData.length; i++){
-            if(AllUserData[i].user !== conn){
-                AllUserData[i].user.sendText(JSON.stringify({
-                    'dataType': 'base64',
-                    'str': str
-                }));
+            obj = JSON.parse(jsonData);
+            if(obj.dataType !== 'base64'){
+                if(AllUserData[i].user !== conn){
+                    obj.cont = myName + '说（' + dateFtt('hh:mm:ss', new Date()) + '）：' + obj.cont;
+                }else{
+                    obj.isMe = true;
+                    obj.cont = '我说（' + dateFtt('hh:mm:ss', new Date()) + '）：' + obj.cont;
+                }
             }
+            AllUserData[i].user.sendText(JSON.stringify(obj));
         }
         // conn.sendText(str.toUpperCase() + i);
     })
     conn.on("close", function (code, reason) {
-        console.log("Connection closed")
+        userNum--;
         // 当用户退出的时候捕捉到退出的用户
         for (var i=0 in AllUserData) {
             if (AllUserData[i].user == conn) {
                 AllUserData.splice(i, 1);
-                console.log("在线用户数量：" + AllUserData.length);
+                for(var i = 0 ; i < AllUserData.length; i++){
+                    sendText = {
+                        dataType: 'onlineUserNum',
+                        onlineUserNum: userNum,
+                        cont: ''
+                    };
+                    sendText.cont = myName + '已退出房间，目前房间人数为' + userNum + '人';
+                    AllUserData[i].user.sendText(JSON.stringify(sendText));
+                }
             }
         }
     })
 }).listen(8001)
+
+
+function dateFtt(fmt,date)   
+{ //author: meizz
+  var o = {   
+    "M+" : date.getMonth()+1,                 //月份
+    "d+" : date.getDate(),                    //日
+    "h+" : date.getHours(),                   //小时
+    "m+" : date.getMinutes(),                 //分
+    "s+" : date.getSeconds(),                 //秒
+    "q+" : Math.floor((date.getMonth()+3)/3), //季度
+    "S"  : date.getMilliseconds()             //毫秒
+  };   
+  if(/(y+)/.test(fmt))   
+    fmt=fmt.replace(RegExp.$1, (date.getFullYear()+"").substr(4 - RegExp.$1.length));   
+  for(var k in o)   
+    if(new RegExp("("+ k +")").test(fmt))   
+  fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));   
+  return fmt;   
+} 
